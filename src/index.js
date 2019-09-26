3b18030abac4bca3c722416fd62cf52756ddba6b
Evan Vogts <evanjvogts@gmail.com>
2019-09-26 13:34:59 -0500

    updated imports

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import shoppingList from './shopping-list.js';
+import './styles/index.css';
+import shoppingList from './shopping-list';
 
 const main = function () {
   shoppingList.bindEventListeners();
